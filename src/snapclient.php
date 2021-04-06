4eb5289f75bf977691df9ff9ad5c122e466f7d4c
Scott Hammel <scott@scotthammel.com>
2021-04-06 15:53:35 -0400

    talking.

--- src/snapclient.php
+++ src/snapclient.php
@@ -1,2 +1,115 @@
 <?php
+
+/*
+ * MOST IMPORTANT NOTE: Exception messages must be read in a sultry, female, robot voice.  Thank you.
+ */
+
+DEFINE('SC_SETUP_KEY','/home/scott/.ssh/scsetup');
+
+class ClaSnapClient
+{
+    private $scsetupkey;
+    private $currentHost;
+    private $scsid;
+    private $sessionData;
+    private $setupMode; // convenient!
+    
+    public function __construct()
+    {
+        $this->setupMode=false;
+        $this->sessionData=array();
+        $this->scsid=null;
+        $this->currentHost='';
+       
+        $this->scsetupkey=file_get_contents(SC_SETUP_KEY);
+        if($this->scsetupkey===false)
+            throw new Exception('Setup key file read failed.');
+    }
+    
+    public function SUP($keyfile,$host,$snappath)
+    {
+        trim($snappath);
+        if($snappath[strlen($snappath)-1]!=='/')
+        {
+            if(strlen($snappath)>0)
+                $snappath=$snappath . '/';
+        }
+        $url='https://' . $host . '/' . $snappath . 'snapserver.php'; 
+        // protocol is:  HLO,SUP,BYE
+        // In this case, HLO and SUP will use the setup key, and BYE will use the new key.
+        $appkey=file_get_contents($keyfile);
+        if($appkey===false)
+        {
+            throw new Exception('I could not read the contents of the application key file.');
+        }
+        
+        //TODO: HLO
+        $args=array('sc_appkey',$appkey);
+    echo "IIII: Executing command SUP\n";
+        $this->execCommand('SUP',$url,$args);
+    echo "IIII: Command execution completed.\n";
+        
+        //TODO: BYE
+    }
+    
+    // $timeout is in seconds.  It's provided as an optional value so that when a backup command is run, it 
+    // can be extended to 300 seconds or more.
+    // $postArgs is passed as-is as the cURL post fields array, but with the following added in:
+    //    sc_session => current scsid
+    //    sc_command => command provided in $cmd
+    private function execCommand($cmd,$url,$postArgs,$timeout=20)
+    {
+        $ch=curl_init($url);
+        if($ch===false)
+            throw new Exception('Error initializing cURL');
+       
+        if(!is_null($this->scsid))
+            $postArgs['sc_session']=$this->scsid;
+        $postArgs['sc_command']=$cmd;
+        $opts=array
+        (
+            CURLOPT_RETURNTRANSFER=>1,          // get the response as the return value
+            CURLOPT_POST=>1,
+            CURLOPT_USERAGENT=>'snapclient',
+            CURLOPT_FOLLOWLOCATION => true,     // follow redirects
+            CURLOPT_SSL_VERIFYHOST => 0,        // don't verify certs and stuff.
+            CURLOPT_SSL_VERIFYPEER => false,    // ditto
+            CURLOPT_TIMEOUT=>$timeout,  
+            CURLOPT_POSTFIELDS=>$postArgs,
+        );    
+            
+        curl_setopt_array($ch,$opts);
+        
+        $rcon=curl_exec($ch); 
+        $emsg=curl_error($ch);
+   print_r($rcon); echo "\n"; print_r($emsg); echo "\n";
+            
+        curl_close($ch);
+        
+    }
+};
+
+// command line mode operation
+if(defined('STDIN'))
+{
+    $sci=new ClaSnapClient();
+    if($argc<2)
+        throw new Exception('Please enter a command with your request.');
+    $command=strtoupper(trim($argv[1]));
+    switch($command)
+    {
+        case 'SUP':
+            if($argc<4)
+                throw new Exception('The SUP command requires 3 arguments: public key file spec, target host IP address or name, and path to snapserver.php on the remote host.');
+            $keyfilespec=$argv[2];
+            $targetHost=$argv[3];
+            $snappath=$argv[4];
+            $sci->SUP($keyfilespec,$targetHost,$snappath);
+            break;
+        default:
+            throw new Exception('Invalid command.');
+            break;
+    }
+    
+}
 ?>
--- src/snapserver.php
+++ src/snapserver.php
@@ -1,2 +1,117 @@
 <?php 
+
+/*
+ * MOST IMPORTANT NOTE: Exception messages must be read in a sultry, female, robot voice.  Thank you.
+ */
+DEFINE('SC_APP_KEY_FILE','sc_app_key.pub');
+DEFINE('SC_SUP_KEY_FILE','sc_sup_key.pub');
+class ClaSnapServer
+{
+  private $setupPubKey;
+  private $appPubKey;
+  private $currentCommand;
+  private $postVars;
+  private $scsid;
+  private $sessionData;
+  public function __construct()
+  {
+     //TODO: move the pub key to a file, and after setup, empty the file such that the remote client can't
+     //      do setup again.
+     $this->setupPubKey=file_get_contents(SC_SUP_KEY_FILE);
+     if($this->setupPubKey===false)
+         throw new Exception('I could not read the setup key file.');
+     if(strlen($this->setupPubKey)==0)
+     {
+         $this->setupPubKey=null;
+         $this->appPubKey=file_get_content(SC_APP_KEY_FILE);
+         if($this->appPubKey===false)
+             throw new Exception('There are no keys available.  You will need to call a technician to help me.');
+     }
+     else 
+         $this->appPubKey=null;
+     $this->currentCommand='NOP';
+     $this->scsid=null;
+     $this->sessionData=array();
+     $this->processPostVars();
+  }
+  
+  public function identifyCommand()
+  {
+    $c=strtoupper(trim($this->postVars['command']));   
+    switch($c)
+    {
+        case 'SUP':
+            echo 'EXECUTE SUP HERE';
+            break;
+            
+        default:
+            throw new Exception("Command $c not recognized");
+            break;
+    }
+    // It's trimmed and uppered and a good command.
+    $this->currentCommand=$c;
+  }
+  
+  public function setSession()
+  {
+      if(!isset($this->postVars['session']))
+          throw new Exception('You must process POST vars before a session may be established.');
+
+      if(session_status()===PHP_SESSION_NONE)
+      {
+          session_start();
+      }
+      
+      if(is_null($this->postVars['session']))
+      {
+          $this->scsid='sc_' . bin2hex(random_bytes(10));
+          $_SESSION[$this->scsid]=array();
+      }
+      else
+          $this->scsid=$this->postVars['session'];
+      
+      if(!isset($_SESSION[$this->scsid]))
+          throw new Exception('Session initialization has failed.');
+      
+      $this->sessionData=$_SESSION[$this->scsid];
+      
+  }
+  // NOTE: this is called by the constructor, so you shouldn't have to.
+  public function processPostVars()
+  {
+      $this->postVars=array
+      (
+          'command'=>'',
+          'session'=>null,
+      );
+      if(isset($_POST['sc_session']))
+          $this->postVars['session']=$_POST['sc_session'];
+      if(isset($_POST['sc_command']))
+          $this->postVars['command']=$_POST['sc_command'];
+      
+      $this->identifyCommand();    
+  }
+    
+  public function remoteSetup()
+  {
+      
+  }
+  public function doCommand()
+  {
+      switch($this->currentCommand)
+      {
+          case 'SUP':
+              $this->remoteSetup();
+              break;
+          default:
+              throw new Exception('Unrecognized command.');
+              break;
+      }
+  }
+};
+
+$ssi=new ClaSnapServer();
+
+$ssi->doCommand();
+
 ?>
