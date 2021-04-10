a7131a7e92cb93e273780e40c5b1953381e9044c
Scott <scott@scotthammel.com>
2021-04-10 19:39:31 -0400

    Added WordPress plugin file

---
+++ src/snapcap_wp.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: SnapCap
+ * Description: This is a WordPress plugin file for the SnapCap backup system.  All it does is provide the means to install/uninstall SnapCap directly through the WordPress Dashboard.  The "plugin" itself does nothing else, and isn't even actually necessary for SnapCap to work.
+ * Plugin URI:  https://github.com/IAmScottCH/snapcap
+ * Version:     2.0.0
+ * Author:      Scott Hammel
+ * License:     MIT
+ **/
+
+
+?>
